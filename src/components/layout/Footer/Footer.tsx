afeffb308d26e172dc2f38fcb3fe72d05065c0ac
Adel Gannem <imadelg14@gmail.com>
2025-03-28 10:30:06 -0400

    feat: footer added

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,3 +1,18 @@
-export const Footer = () => {
-	return <div>Footer</div>;
+import { Link } from "react-router-dom";
+
+export const Footer = ({ routes }: RoutesProps) => {
+	return (
+		<div className="flex justify-center p-10 ">
+			<div className="text-[12px] text-gray-500 flex justify-around w-96">
+				<label>Z0© 2025. All Rights Reserved</label>
+				<div className="flex gap-5">
+					{routes.map((item, key) => (
+						<Link to={item.path} key={key}>
+							{item.name}
+						</Link>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
