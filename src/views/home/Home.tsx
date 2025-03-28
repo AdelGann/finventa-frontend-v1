460989f6ea77d69438009f4ace9abc600137aaaa
Adel Gannem <imadelg14@gmail.com>
2025-03-28 10:28:18 -0400

    feat: landing components added

---
+++ src/views/home/Home.tsx
@@ -0,0 +1,15 @@
+import { About } from "./components/About/About.component";
+import { Hero } from "./components/Hero/Hero.component";
+import { Questions } from "./components/Q&A/Q&A.component";
+// import { Pricing } from "./components/Pricing/Pricing.component";
+
+export const Home = () => {
+	return (
+		<div className="p-10 m-10">
+			<Hero />
+			{/* <Pricing />  Descomentar despues*/}
+			<About />
+			<Questions />
+		</div>
+	);
+};
