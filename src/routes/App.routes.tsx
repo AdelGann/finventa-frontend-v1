430c81149831e0d6ed789ec4ebe184dd4ba08f69
Adel Gannem <imadelg14@gmail.com>
2025-03-28 22:26:25 -0400

    feat: login and register routes seted

--- src/routes/App.routes.tsx
+++ src/routes/App.routes.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing } from "./Layouts/Landing/Landing.layout";
+
 import { Home } from "@/views/home/Home";
 import { NotFound } from "@/views/404/404";
+
+import { Login } from "@/views/login/Login";
+import { Register } from "@/views/Register/Register";
 
 export const AppRoutes = () => {
 	return (
@@ -9,11 +13,10 @@ export const AppRoutes = () => {
 			<Routes>
 				<Route element={<Landing />}>
 					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Home />} />
-					<Route path="/register" element={<Home />} />
-					
 					<Route path="*" element={<NotFound />} />
 				</Route>
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
 			</Routes>
 		</BrowserRouter>
 	);
