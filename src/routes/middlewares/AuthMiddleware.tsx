3432f2ac78360344759da1a135a1e7bb6b1bb290
Adel Gannem <imadelg14@gmail.com>
2025-03-28 22:46:01 -0400

    feat: add AuthMiddleware

--- src/routes/middlewares/AuthMiddleware.tsx
+++ src/routes/middlewares/AuthMiddleware.tsx
@@ -1,7 +1,7 @@
 import { useAuthStore } from "@/store/AuthState/AuthStore";
 import { Navigate, Outlet } from "react-router-dom";
 
-export const useAuth = () => {
+export const useAuthMiddleware = () => {
 	const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 	if (isAuthenticated) {
 		return <Outlet />;
