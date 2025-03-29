d52150fafa7007caf0f2aa0bb0fb530f84e53962
Adel Gannem <imadelg14@gmail.com>
2025-03-28 22:15:52 -0400

    feat: 404 Page added

--- src/views/404/404.tsx
+++ src/views/404/404.tsx
@@ -1,8 +1,16 @@
+
+import { motion } from "framer-motion";
 export const NotFound = () => {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen">
+		<motion.section
+			initial={{ opacity: 0, y: 160 }}
+			animate={{ opacity: 1, y: 250 }}
+			transition={{ duration: 0.6 }}
+			className="flex flex-col items-center justify-center"
+		>
+		
 			<h1 className="text-6xl font-bold">404</h1>
 			<p className="mt-4 text-xl">Página no encontrada</p>
-		</div>
+		</motion.section>
 	);
 };
