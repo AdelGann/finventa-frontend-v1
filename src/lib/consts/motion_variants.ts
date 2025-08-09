aaa2ccadfe07bdc6b8b79d5cf353726a40a6a2bd
AdelGann <imadelg14@gmail.com>
2025-08-09 17:35:02 -0400

    feat(layout): fix sidebar animation

--- src/lib/consts/motion_variants.ts
+++ src/lib/consts/motion_variants.ts
@@ -1,33 +1,33 @@
 export const textVariants = {
-	open: {
-		opacity: 1,
-		width: "auto",
-		x: 0,
-		transition: { duration: 0.3 },
-	},
-	closed: {
-		opacity: 0,
-		width: 0,
-		x: -10,
-		transition: { duration: 0.3 },
-	},
-	invisible: { opacity: 0, x: -20, transition: { duration: 0.2 } },
-	visible: { opacity: 1, x: 0, transition: { duration: 0.2 } },
+  open: {
+    opacity: 1,
+    maxWidth: 200, // Ajusta según tu diseño
+    x: 0,
+    transition: { duration: 0.3 },
+  },
+  closed: {
+    opacity: 0,
+    maxWidth: 0,
+    x: -10,
+    transition: { duration: 0.3 },
+  },
+  invisible: { opacity: 0, x: -20, transition: { duration: 0.2 } },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.2 } },
 };
 
 export const badgeVariants = {
-	open: {
-		opacity: 1,
-		width: "auto",
-		marginLeft: 0,
-		transition: { duration: 0.3 },
-	},
-	closed: {
-		opacity: 0,
-		width: 0,
-		marginLeft: 0,
-		transition: { duration: 0.3 },
-	},
-	invisible: { opacity: 0, scale: 0.8, transition: { duration: 0.2 } },
-	visible: { opacity: 1, scale: 1, transition: { duration: 0.2 } },
+  open: {
+    opacity: 1,
+    maxWidth: 200, // Ajusta según tu diseño
+    x: 0,
+    transition: { duration: 0.3 },
+  },
+  closed: {
+    opacity: 0,
+    maxWidth: 0,
+    x: -10,
+    transition: { duration: 0.3 },
+  },
+  invisible: { opacity: 0, scale: 0.8, transition: { duration: 0.2 } },
+  visible: { opacity: 1, scale: 1, transition: { duration: 0.2 } },
 };
