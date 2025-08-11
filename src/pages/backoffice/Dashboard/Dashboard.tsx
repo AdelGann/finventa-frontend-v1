8dec9dad2f3c36d1ea5d7795858a2ae857ce116d
AdelGann <imadelg14@gmail.com>
2025-08-10 23:23:06 -0400

    feat(pages): refactor dashboard tabs

--- src/pages/backoffice/Dashboard/Dashboard.tsx
+++ src/pages/backoffice/Dashboard/Dashboard.tsx
@@ -1,51 +1,78 @@
 import { lazy } from "react";
-import { SEO } from "@/components/custom/SEO";
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { DollarSign, CreditCard } from "lucide-react"; // Importación de íconos
-import { SuspenseWrapper } from "@/components/custom/Suspense-wrapper";
 import Charts from "./components/Charts";
+import { SEO } from "@/components/custom/SEO";
+import { Tabs } from "@/components/custom/Tabs";
+import { SuspenseWrapper } from "@/components/custom/Suspense-wrapper";
+
+import { DollarSign, CreditCard } from "lucide-react"; // Importación de íconos
 
 const Income = lazy(() => import("./views/Income"));
 const Debts = lazy(() => import("./views/Debts"));
 
 const Dashboard = () => {
-  return (
-    <>
-      <SEO
-        title="Z0 - Dashboard"
-        keywords="dashboard, manage system"
-        description="Welcome to the control panel. Here you can manage and view all the important information."
-      />
-      <div className="w-full">
-        <div className="flex justify-center flex-col">
-          <Tabs defaultValue="incomes">
-            <TabsList className="w-[90vw] sm:w-[50vw] mx-auto">
-              <TabsTrigger value="incomes" className="cursor-pointer">
-                <DollarSign className="mr-2 h-4 w-4 text-green-700" /> Incomes
-              </TabsTrigger>
-              <TabsTrigger value="debts" className="cursor-pointer">
-                <CreditCard className="mr-2 h-4 w-4 text-red-700" /> Debts
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="incomes" className="flex justify-center w-full">
-              <SuspenseWrapper>
-                <Income />
-              </SuspenseWrapper>
-            </TabsContent>
-            <TabsContent value="debts" className="flex justify-center w-full">
-              <SuspenseWrapper>
-                <Debts />
-              </SuspenseWrapper>
-            </TabsContent>
-          </Tabs>
-          <div className="h-[40vh]">
-            <Charts />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<SEO
+				title="Z0 - Dashboard"
+				keywords="dashboard, manage system"
+				description="Welcome to the control panel. Here you can manage and view all the important information."
+			/>
+			<div className="w-full">
+				<div className="flex justify-center flex-col">
+					<Tabs
+						defaultValue="incomes"
+						tabListClassName="w-[90vw] sm:w-[50vw] mx-auto"
+						tabsTrigger={tab_trigger}
+						tabsContent={tabs_content}
+					/>
+					<div>
+						<Charts />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
+const tab_trigger = [
+	{
+		value: "incomes",
+		className: "cursor-pointer",
+		children: (
+			<>
+				<DollarSign className="mr-2 h-4 w-4 text-green-700" /> Incomes
+			</>
+		),
+	},
+	{
+		value: "debts",
+		className: "cursor-pointer",
+		children: (
+			<>
+				<CreditCard className="mr-2 h-4 w-4 text-red-700" /> Debts
+			</>
+		),
+	},
+];
+
+const tabs_content = [
+	{
+		value: "incomes",
+		className: "flex justify-center w-full",
+		children: (
+			<SuspenseWrapper>
+				<Income />
+			</SuspenseWrapper>
+		),
+	},
+	{
+		value: "debts",
+		className: "flex justify-center w-full",
+		children: (
+			<SuspenseWrapper>
+				<Debts />
+			</SuspenseWrapper>
+		),
+	},
+];
 
 export default Dashboard;
