72157c48addcdb329d9eeae0ab3f5e5b6f2a542d
AdelGann <imadelg14@gmail.com>
2025-10-25 17:46:30 -0400

    fix: inputs styles

--- src/components/custom/InputField.tsx
+++ src/components/custom/InputField.tsx
@@ -1,16 +1,22 @@
-import { Input } from '../ui/input';
-import { Label } from '../ui/label';
+import { cn } from "@/lib/utils";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 
-interface InputFieldProps extends React.ComponentProps<'input'> {
+interface InputFieldProps extends React.ComponentProps<"input"> {
   label?: string;
   labelClassName?: string;
 }
 
-export const InputField = ({ label, labelClassName, ...rest }: InputFieldProps) => {
+export const InputField = ({
+  label,
+  labelClassName,
+  className,
+  ...rest
+}: InputFieldProps) => {
   return (
-    <>
+    <div className="flex flex-col gap-2">
       {label && <Label children={label} className={labelClassName} />}
-      <Input {...rest} />
-    </>
+      <Input className={cn("w-full", className)} {...rest} />
+    </div>
   );
 };
--- src/components/custom/SelectField.tsx
+++ src/components/custom/SelectField.tsx
@@ -1,74 +1,78 @@
 import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectLabel,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "../ui/select";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import { Label } from "../ui/label";
 
 import Loader from "./Loader";
+import { cn } from "@/lib/utils";
 
-interface SelectFieldProps extends React.ComponentProps<typeof SelectPrimitive.Root> {
-	label?: string;
-	labelClassName?: string;
-	placeholder?: string;
-	className?: string;
-	contentClassName?: string;
-	options: { [key: string]: string }[] | Record<string, { [key: string]: string }[]>;
-	option_name?: string;
-	option_value?: string;
-	isLoading?: boolean;
+interface SelectFieldProps
+  extends React.ComponentProps<typeof SelectPrimitive.Root> {
+  label?: string;
+  labelClassName?: string;
+  placeholder?: string;
+  className?: string;
+  contentClassName?: string;
+  options:
+    | { [key: string]: string }[]
+    | Record<string, { [key: string]: string }[]>;
+  option_name?: string;
+  option_value?: string;
+  isLoading?: boolean;
 }
 
 const SelectField = ({
-	label,
-	labelClassName = "text-sm font-medium",
-	className = "w-full",
-	contentClassName,
-	options,
-	option_name = "name",
-	option_value = "value",
-	placeholder,
-	isLoading = false,
-	...rest
+  label,
+  labelClassName = "text-sm font-medium",
+  className,
+  contentClassName,
+  options,
+  option_name = "name",
+  option_value = "value",
+  placeholder,
+  isLoading = false,
+  ...rest
 }: SelectFieldProps) => {
-	const data_options = Array.isArray(options)
-		? [{ label: null, options }]
-		: Object.entries(options).map(([key, value]) => ({
-				label: key,
-				options: value,
-		  }));
+  const data_options = Array.isArray(options)
+    ? [{ label: null, options }]
+    : Object.entries(options).map(([key, value]) => ({
+        label: key,
+        options: value,
+      }));
 
-	return (
-		<>
-			<Label className={labelClassName}>{label}</Label>
-			<Select {...rest}>
-				<SelectTrigger className={className}>
-					<SelectValue placeholder={placeholder} />
-				</SelectTrigger>
-				<SelectContent className={contentClassName}>
-					{!isLoading ? (
-						data_options.map(({ label, options }, key) => (
-							<SelectGroup key={key}>
-								{label ? <SelectLabel key={key}>{label}</SelectLabel> : null}
-								{options.map((item, key) => (
-									<SelectItem value={item[option_value]} key={`item-${key}`}>
-										{item[option_name]}
-									</SelectItem>
-								))}
-							</SelectGroup>
-						))
-					) : (
-						<Loader divClassName="flex items-center justify-center" />
-					)}
-				</SelectContent>
-			</Select>
-		</>
-	);
+  return (
+    <div className="flex flex-col gap-2">
+      <Label className={labelClassName}>{label}</Label>
+      <Select {...rest}>
+        <SelectTrigger className={cn("w-full", className)}>
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent className={contentClassName}>
+          {!isLoading ? (
+            data_options.map(({ label, options }, key) => (
+              <SelectGroup key={key}>
+                {label ? <SelectLabel key={key}>{label}</SelectLabel> : null}
+                {options.map((item, key) => (
+                  <SelectItem value={item[option_value]} key={`item-${key}`}>
+                    {item[option_name]}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            ))
+          ) : (
+            <Loader divClassName="flex items-center justify-center" />
+          )}
+        </SelectContent>
+      </Select>
+    </div>
+  );
 };
 
 export default SelectField;
