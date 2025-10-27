43df31d0ab9b8733b3993816456e99c6265a787a
AdelGann <imadelg14@gmail.com>
2025-10-26 21:26:25 -0400

    fix: style

--- src/pages/backoffice/Products/views/ProductSheet.tsx
+++ src/pages/backoffice/Products/views/ProductSheet.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { ImagePreviewUpload } from "@/components/custom/ImagePreviewUpload";
 import { ProductData } from "../components/ProductCard";
+import SelectField from "@/components/custom/SelectField";
 
 export const ProductSheet = ({
   state,
@@ -32,41 +33,49 @@ export const ProductSheet = ({
 
         <div className="flex flex-col justify-between p-3 h-full">
           {/* Imagen */}
-          <div className="mb-4">
-            <ImagePreviewUpload
-              imageUrl={data.imageUrl}
-              onImageChange={handleImageChange}
-            />
+          <div>
+            <div className="mb-4">
+              <ImagePreviewUpload
+                imageUrl={data.imageUrl}
+                onImageChange={handleImageChange}
+              />
+            </div>
+
+            {/* Título */}
+            <div>
+              <InputField label="Título" defaultValue={data.title} />
+            </div>
           </div>
 
-          {/* Título */}
-          <div className="mb-4">
-            <InputField label="Título" defaultValue={data.title} />
-          </div>
+          <div className="flex flex-col gap-2">
+            <Separator className="my-4" />
+            {/* Descripción */}
+            <div>
+              <InputField label="Descripción" defaultValue={data.description} />
+            </div>
 
-          <Separator className="my-4" />
-
-          {/* Descripción */}
-          <div className="mb-4">
-            <InputField label="Descripción" defaultValue={data.description} />
-          </div>
-
-          {/* Cantidad y Precio */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
-            <InputField
-              label="Cantidad"
-              type="number"
-              defaultValue={data.quantity}
-            />
-            <InputField
-              label="Precio"
-              type="number"
-              defaultValue={data.price}
-            />
+            <Separator className="my-4" />
+            {/* Cantidad y Precio */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+              <InputField
+                label="Cantidad"
+                type="number"
+                defaultValue={data.quantity}
+              />
+              <InputField
+                label="Precio"
+                type="number"
+                defaultValue={data.price}
+              />
+            </div>
+            <Separator className="my-4" />
+            <div>
+              <SelectField label="Estado:" options={[]} />
+            </div>
+            <Separator className="my-4" />
           </div>
 
           {/* Botones */}
-          <Separator className="my-4" />
           <div className="flex justify-end gap-2">
             <Button variant="secondary" onClick={() => setState(false)}>
               Cancelar
